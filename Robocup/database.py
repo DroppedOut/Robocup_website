d05d3622aebfc3361055132fde11b928abed00b4
IceDalek <kostyan40k@mail.ru>
2020-06-16 19:19:30 +0300

    add images

--- Robocup/database.py
+++ Robocup/database.py
@@ -3,7 +3,9 @@ import sqlite3
 conn = sqlite3.connect("data.db")
 cur = conn.cursor()
 
-query = "CREATE TABLE IF NOT EXISTS teams (name TEXT, mem1 TEXT, mem2 TEXT, mem3 TEXT, mem4 TEXT, mentor TEXT, league TEXT, event TEXT)"
+query = "CREATE TABLE IF NOT EXISTS teams (name TEXT, mem1 TEXT, mem2 TEXT, mem3 TEXT, mem4 TEXT, mentor TEXT, league TEXT,\
+event TEXT)"
 cur.execute(query)
-
+query = "CREATE TABLE IF NOT EXISTS refrees (name TEXT, secondName TEXT, league TEXT) "
+cur.execute(query)
         
